ccf7a063e3b67dd4438379644447485aee1fb479
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-29 17:44:34 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as txt:
+    long_description = txt.read()
+
+setuptools.setup(
+    name='AutoPostTelegram',
+    version='0.0.01',
+    description='An Telegram Auto Post Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='Aasfcyberking',
+    author_email='aasfvl@gmail.com',
+    url='https://telegram.me/Aasf_CyberKing',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Typing :: Typed'
+    ],
+    install_requires= ['requests'],
+    python_requires='>=3.6'
+)
