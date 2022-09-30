6cc222297c39d3d847c5573e8e1f94c1fb407878
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-30 12:29:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as txt:
 
 setuptools.setup(
     name='AutoPostTelegram',
-    version='0.0.3',
+    version='0.0.5',
     description='An Telegram Auto Post Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
